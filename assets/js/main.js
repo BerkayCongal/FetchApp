33851ac4f1900b93f668fd59c3ee59591c667b3b
Berkay Çongal <berkaycongal@hotmail.com>
2023-08-18 17:24:38 +0300

    dialog eklendi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,77 @@
- const postContainer = document.querySelector(".container")
+const postContainer = document.querySelector(".container");
+const dialog = document.querySelector(".dialog");
 
 const getData = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => renderPosts(json))
-}
-
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => renderPosts(json));
+};
 
 getData();
 
+dialog.addEventListener("click", () => {
+  
+});
 
-
-function removeİtems(container) {
-        while (container.firstChild) {
-            container.removeChild(container.firstChild)
-        }
+function removeItems(container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
 
-const renderPosts = (datas) =>  {
-    datas.forEach(data => {
-        console.log(data)
+const renderPosts = (datas) => {
+  datas.forEach((data) => {
+    console.log(data);
 
+    const divEl = document.createElement("div");
+    const spanEl = document.createElement("span");
+    const commets = document.createElement("button");
+    const commentsContainer = document.createElement("div");
 
-        const divEl = document.createElement("div");
-        const buttonEl = document.createElement("button");
-        const spanEl = document.createElement("span");
-        const commets = document.createElement("button")
-        const commentsContainer = document.createElement("div")
-       
+    divEl.classList.add("containerdiv");
+    commentsContainer.classList.add(".commetUser");
+    commets.classList.add("buttonComments");
 
-        divEl.classList.add("containerdiv")
-        commentsContainer.classList.add(".commetUser")
-        buttonEl.classList.add("buttonUser")
-        commets.classList.add("buttonComments")
-        
-    
-        buttonEl.addEventListener("click", () => {
-            fetch(`https://jsonplaceholder.typicode.com/users/${data.userId}`)
-            .then(response => response.json())
-            .then(result => {
-                
-                spanEl.innerText = result.name;
-                divEl.appendChild(spanEl)
-            })
-        })
+    commets.addEventListener("click", () => {
+      dialog.showModal();
+      console.log(commets);
+      removeItems(dialog);
+      fetch(`https://jsonplaceholder.typicode.com/comments/?postId=${data.id}`)
+        .then((response) => response.json())
+        .then((comments) => {
+          comments.forEach((comment) => {
+            const spancomments = document.createElement("span");
+            spancomments.classList.add("span-comment");
+            spancomments.innerText = `
+            Explanation: ${comment.name}
+            Email : ${comment.email}
+            Comment: ${comment.name}
+            `
+            dialog.appendChild(spancomments);
+          });
 
-        commets.addEventListener("click", () => {
-
-            removeİtems(commentsContainer)
-            fetch(`https://jsonplaceholder.typicode.com/comments/?postId=${data.id}`)
-            .then(response => response.json())
-            .then(comments => { 
-
-                comments.forEach(comment => {
-                    const spancomments = document.createElement("span")
-                    spancomments.classList.add("span-comment")
-                    spancomments.innerText = comment.name
-                    commentsContainer.appendChild(spancomments);
-                }) 
-            })
-        })
-
-        commets.innerText = "Comments In"
-        buttonEl.innerText = "User In"
-
-        divEl.innerText = `${data.id} ${data.title}`;
-        divEl.appendChild(commentsContainer)
-        divEl.appendChild(commets)
-        divEl.appendChild(buttonEl)
-        postContainer.appendChild(divEl)
+          fetch(`https://jsonplaceholder.typicode.com/users/${data.userId}`)
+            .then((response) => response.json())
+            .then((result) => {
+              const spanuser = document.createElement("span");
+              spanuser.classList.add("span-user");
+              spanuser.innerText = `
+              Name: ${result.name}
+              Username: ${result.username}
+              Phone:${result.phone}
+              Email:${result.email}
+              `;
+              dialog.appendChild(spanuser);
+            });
+        });
+        dialog.showModal();
     });
-}
+
+    
+    commets.innerText = "";
+    divEl.innerText = `${data.id}- Title : ${data.title}`;
+    divEl.appendChild(commentsContainer);
+    divEl.appendChild(commets);
+    postContainer.appendChild(divEl);
+  });
+};
